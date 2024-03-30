74612d725b9ba15cb3a33ae337f83acac99d7a3e
Norman Lumilaan <norman.lumilaan@gmail.com>
2024-03-30 14:21:17 +0200

    Add functioning clock to menubar

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 import "./aqua.css";
+
+function menubarClock(clockElem: Element | null): void {
+  if (clockElem === null) return;
+
+  function update(): void {
+    const time = new Date().toLocaleString("en-US", {
+      weekday: "short",
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+    if (clockElem) {
+      clockElem.textContent = time;
+    }
+  }
+
+  update(); // Update immediately when the function is called
+  setInterval(update, 60000); // Update every minute
+}
+
+menubarClock(document.querySelector("#menubar-clock"));
